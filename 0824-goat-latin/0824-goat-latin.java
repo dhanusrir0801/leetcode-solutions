7e6f2f8746c424c9369f402c4294de0f91c97248
DHANUSRI R <dhanusrir0801@gmail.com>
2026-09-25 23:55:07 +0530

    Time: 6 ms (30.65%), Space: 43.8 MB (34.97%) - LeetHub

---
+++ 0824-goat-latin/0824-goat-latin.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String toGoatLatin(String sentence) {
+
+        String[] words = sentence.split(" ");
+        StringBuilder answer = new StringBuilder();
+
+        String vowels = "aeiouAEIOU";
+
+        for (int i = 0; i < words.length; i++) {
+
+            String word = words[i];
+
+            if (vowels.indexOf(word.charAt(0)) == -1) {
+                word = word.substring(1) + word.charAt(0);
+            }
+
+            word += "ma";
+
+            for (int j = 0; j <= i; j++) {
+                word += "a";
+            }
+
+            if (i > 0) {
+                answer.append(" ");
+            }
+
+            answer.append(word);
+        }
+
+        return answer.toString();
+    }
+}
